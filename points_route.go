dce67f27c31a3e8132175eadce1501988f20c805
Madson Cardoso <448807+madson@users.noreply.github.com>
2021-03-23 16:51:32 -0300

    Change implementation to deal with big integers

--- points_route.go
+++ points_route.go
@@ -1,60 +1,63 @@
 package main
 
 import (
+	"fmt"
+	"math/big"
 	"net/http"
 	"sort"
-	"strconv"
 )
 
 func (s server) handlePointsGet() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		params := r.URL.Query()
 
-		x, err := strconv.ParseInt(params.Get("x"), 10, 64)
-		if err != nil {
-			error := messageResponse{
+		x := big.NewInt(0)
+		y := big.NewInt(0)
+		distance := big.NewInt(0)
+
+		if _, ok := x.SetString(params.Get("x"), 10); ok == false || x.IsInt64() == false {
+			err := messageResponse{
 				Status:  400,
-				Message: "invalid URL query param: x",
+				Message: "invalid value for x axis",
 			}
-			s.respond(w, r, error, 400)
+			s.respond(w, r, err, 400)
 			return
 		}
 
-		y, err := strconv.ParseInt(params.Get("y"), 10, 64)
-		if err != nil {
-			error := messageResponse{
+		if _, ok := y.SetString(params.Get("y"), 10); ok == false || y.IsInt64() == false {
+			err := messageResponse{
 				Status:  400,
-				Message: "invalid URL query param: y",
+				Message: "invalid value for x axis",
 			}
-			s.respond(w, r, error, 400)
+			s.respond(w, r, err, 400)
 			return
 		}
 
-		distance, err := strconv.ParseInt(params.Get("distance"), 10, 64)
-		if err != nil {
-			error := messageResponse{
+		if _, ok := distance.SetString(params.Get("distance"), 10); ok == false || distance.IsInt64() == false {
+			err := messageResponse{
 				Status:  400,
-				Message: "invalid URL query param: distance",
+				Message: "invalid value for distance",
 			}
-			s.respond(w, r, error, 400)
+			s.respond(w, r, err, 400)
 			return
 		}
 
-		if distance < 0 {
-			error := messageResponse{
+		if distance.Int64() < 0 {
+			err := messageResponse{
 				Status:  400,
 				Message: "distance param can't be negative",
 			}
-			s.respond(w, r, error, 400)
+			s.respond(w, r, err, 400)
 			return
 		}
 
 		pointOrigin := point{
-			X: x,
-			Y: y,
+			X: x.Int64(),
+			Y: y.Int64(),
 		}
 
-		pointsNearBy := s.pointsNearBy(pointOrigin, distance)
+		// handling distances as strings to support numbers greater than MaxInt64
+		pointsNearBy := s.pointsNearBy(pointOrigin, distance.String())
 
 		response := pointsResponse{
 			Count:  len(pointsNearBy),
@@ -66,21 +69,25 @@ func (s server) handlePointsGet() http.HandlerFunc {
 	}
 }
 
-func (s server) pointsNearBy(origin point, distance int64) []pointDistance {
+func (s server) pointsNearBy(origin point, distance string) []pointDistance {
 	results := make([]pointDistance, 0)
 
+	distance = fmt.Sprintf("%020s", distance)
+
 	for _, current := range *s.data {
-		if d := current.distanceFrom(origin); d <= distance {
+		// handling distances as leading 0 strings to make lessOrEqual operator work correctly
+		if d := current.distanceFrom(origin); fmt.Sprintf("%020s", d.String()) <= distance {
 			pd := pointDistance{
 				Point:    current,
-				Distance: d,
+				Distance: d.String(),
 			}
 			results = append(results, pd)
 		}
 	}
 
 	sort.Slice(results, func(i, j int) bool {
-		return results[i].Distance < results[j].Distance
+		// handling distances as leading 0 strings to make less operator work correctly
+		return fmt.Sprintf("%020s", results[i].Distance) < fmt.Sprintf("%020s", results[j].Distance)
 	})
 
 	return results
