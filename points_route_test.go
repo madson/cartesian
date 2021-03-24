240507fe5e31b6cdfa566e98790e7df3458aac3d
Madson Cardoso <448807+madson@users.noreply.github.com>
2021-03-24 20:13:11 -0300

    Add very few tests for endpoint /api/points

--- points_route_test.go
+++ points_route_test.go
@@ -2,16 +2,130 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func Test_server_handlePointsGet(t *testing.T) {
+	t.Run("handlePointsGet no URL params", func(t *testing.T) {
+		server := newServer()
 
+		req, err := http.NewRequest(http.MethodGet, "/api/points", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(server.handlePointsGet())
+
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+		}
+	})
+
+	t.Run("handlePointsGet invalid value for x axis", func(t *testing.T) {
+		server := newServer()
+
+		req, err := http.NewRequest(http.MethodGet, "/api/points", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		tests := []string{
+			"invalid",
+			"*",
+			" ",
+		}
+
+		for _, value := range tests {
+			q := req.URL.Query()
+			q.Add("x", value)
+			q.Add("y", "0")
+			q.Add("distance", "1000")
+			req.URL.RawQuery = q.Encode()
+
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(server.handlePointsGet())
+
+			handler.ServeHTTP(rr, req)
+
+			if status := rr.Code; status != http.StatusBadRequest {
+				t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+			}
+		}
+	})
+
+	t.Run("handlePointsGet invalid value for y axis", func(t *testing.T) {
+		server := newServer()
+
+		req, err := http.NewRequest(http.MethodGet, "/api/points", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		tests := []string{
+			"invalid",
+			"*",
+			" ",
+		}
+
+		for _, value := range tests {
+			q := req.URL.Query()
+			q.Add("x", "0")
+			q.Add("y", value)
+			q.Add("distance", "1000")
+			req.URL.RawQuery = q.Encode()
+
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(server.handlePointsGet())
+
+			handler.ServeHTTP(rr, req)
+
+			if status := rr.Code; status != http.StatusBadRequest {
+				t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+			}
+		}
+	})
+
+	t.Run("handlePointsGet invalid value for distance", func(t *testing.T) {
+		server := newServer()
+
+		req, err := http.NewRequest(http.MethodGet, "/api/points", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		tests := []string{
+			"invalid",
+			"*",
+			" ",
+		}
+
+		for _, value := range tests {
+			q := req.URL.Query()
+			q.Add("x", "0")
+			q.Add("y", "0")
+			q.Add("distance", value)
+			req.URL.RawQuery = q.Encode()
+
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(server.handlePointsGet())
+
+			handler.ServeHTTP(rr, req)
+
+			if status := rr.Code; status != http.StatusBadRequest {
+				t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+			}
+		}
+	})
 }
 
 func Test_server_pointsNearBy(t *testing.T) {
 
-	t.Run("pointsNearBy len", func(t *testing.T) {
+	t.Run("pointsNearBy results count", func(t *testing.T) {
 		server := newServer()
 		*server.data = []point{
 			{X: -59, Y: 38},
